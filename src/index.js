71a3fa35220fcdc07f2784cb5960384fb2db5688
Maciek Bednarski <mBednar2020@gmail.com>
2022-03-02 00:27:25 +0100

    init commit with router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AboutMe from './components/aboutMe';
+import Container from '@mui/material/Container';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Container>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<App />} />
+                    <Route path='about' element={<AboutMe />} />
+                    {/* <Route path='projects' element={<Invoices />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </Container>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
